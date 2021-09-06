947f53dc1b7450302268d61d38a66344d0ffb0f9
vascYT <nicovanaschaufler@gmail.com>
2021-09-06 00:08:39 +0000

    Deploying to gh-pages from @ Vegacy/vegacy.de@35597a4f380d82f087ec977c2197055bda0a6419 🚀

--- _next/static/lOcppk9VehCrQJWwJK7ev/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d117bdef0d6fc6e4093b.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/[redirect]":["static/chunks/pages/[redirect]-974e43dd1fbc9b36aaac.js"],sortedPages:["/","/_app","/_error","/[redirect]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/yxt_GA1jYNjb_KDTo4VQO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2da368f9cc85d7a1d269.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/[redirect]":["static/chunks/pages/[redirect]-974e43dd1fbc9b36aaac.js"],sortedPages:["/","/_app","/_error","/[redirect]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
